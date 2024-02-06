36ad15da04ed70b238c48c1c8f0abb5da081c90f
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2024-02-06 08:08:09 +0800

    feat: partial CRUD for admin users

---
+++ app/Http/Requests/UpdateAdminRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateAdminRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name" => "sometimes|required|max:150",
+            "password" => "sometimes|required|max:200",
+            "is_super_admin" => "sometimes|boolean",
+            "is_active" => "sometimes|boolean",
+        ];
+    }
+}
